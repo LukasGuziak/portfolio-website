760f3a92329a7b183552acdb71ec8d002dcd5c13
Lukasz Guziak <guziak.luk@gmail.com>
2022-10-03 17:00:26 +0200

    add burger behavior

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
+const burgerBtn = document.querySelector('.hamburger');
+const headerSection = document.querySelector('.header');
+const logo = document.querySelector('.logo');
 
+const hendleNav = () => {
+	burgerBtn.classList.toggle('is-active');
+	headerSection.classList.toggle('header--active');
+	logo.classList.toggle('logo--active');
+};
+
+burgerBtn.addEventListener('click', hendleNav);
+headerSection.addEventListener('click', hendleNav);
